008e97f258a83aab9374d323fc201c6c122d9d1f
Chelle Wood <chelleyoungw@gmail.com>
2024-02-07 00:20:40 -0600

    added js to take the form input city name and when search is clicked will add it to the search history and make a request to the api and all weather for that city is logged into the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
-var requestURL = "http://api.openweathermap.org/data/2.5/forecast?lat=29.699877315364983&lon=-98.15306328187403&appid=347b927f936256c8ed77f0381cc6d59a"
+var cityNameSearchEl = $("#city-name");
+var cityFormSearchEl = $("#city-form");
+var citySearchHistoryEl = $("#city-search-history");
 
-fetch(requestURL)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-})
+//Handles form submission for City Search
+function sendCityNameRequest(event) {
+    //Prevent the default behavior
+    event.preventDefault();
+
+    //Select form element by the name attribute and get its value
+    var cityName = $("input[id= city-name]").val();
+
+    //if there is nothing in the form entered, don't print to the page
+    if (!cityName) {
+        console.log("No city name filled out in form!");
+        return;
+    }
+
+    //print to the page
+    citySearchHistoryEl.append("<li>" + cityName + "</li>");
+
+    //clear the form input element
+    $("input[name= city-name]").val("");
+
+    var apiKey = "347b927f936256c8ed77f0381cc6d59a";
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+}
+
+//Create a submit event listener on the form element
+cityFormSearchEl.on("submit", sendCityNameRequest);
